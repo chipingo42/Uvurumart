6734b8214ff0124d8245c30041609a9e64167489
chipingo42 <akalazuvictor42@.com>
2021-07-30 13:38:21 +0100

    first commit

---
+++ src/main.js/index.js
@@ -0,0 +1,13 @@
+var hamburger, ul
+ = document.querySelector("#hamburger")
+
+ =document.querySelector(".hamburger").addEventListener("click", () => {
+    formWrap.style.display = "block"
+    console.log('clicked...') 
+})
+console.log(hamburger)
+
+
+document.querySelector('#close-button ').addEventListener('click', function(){
+    let x = document.querySelector('.close-button').style.display = 'none';
+})
