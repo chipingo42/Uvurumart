c6d47da2409e4f6c363276669a06aa6d9a591b88
chipingo42 <akalazuvictor42@.com>
2021-08-09 11:10:22 +0100

    javascript functoin

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+var hamburger, poping,
+poping = document.querySelector("#hamburger")
+
+hamburger=document.querySelector(".hamburger").addEventListener("click", () => {
+    poping.style.display = "block"
+    console.log('clicked...') 
+})
+console.log(poping);
+
+
+document.querySelector('.close-button svg').addEventListener('click', function(){
+    let x = document.querySelector('.close-button').style.display = 'none';
+})
+
